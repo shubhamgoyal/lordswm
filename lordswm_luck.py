20d72c3a15972dcefb6f59014e4794abb598e437
shubham <shubhamjigoyal@gmail.com>
2013-12-09 09:37:43 +0800

    Prevented probabilities > 1 and used these to take care of invalid sequences

--- lordswm_luck.py
+++ lordswm_luck.py
@@ -1,6 +1,7 @@
 import itertools
+import math
 
-TOTAL_TURNS = 10
+TOTAL_TURNS = 14
 LUCK = 2
 
 class unique_element:
@@ -28,21 +29,30 @@ def perm_unique_helper(listunique,result_list,d):
 
 def get_prob(luck, turn_number, turns_befallen):
 	F = min(0.5, luck/10.0)
-	return F**(1 + turns_befallen - (turn_number - turns_befallen - 1)*F/(1 - F))
+	prob = F**(1 + turns_befallen - (turn_number - turns_befallen - 1)*F/(1 - F))
+	# if prob > 1:
+	# 	print "Probability greater than 1: prob = ", prob, " luck = ", luck, " turn_number = ", turn_number, " turns_befallen = ", turns_befallen
+	return prob
 
-def get_prob_sequence(sequence):
+def get_prob_sequence(sequence, num_luck_falls):
 	final_prob = 1.0
 	num_previous_luck_falls = 0
 	for i in range(1, TOTAL_TURNS + 1):
 		element = sequence[i - 1]
-		if element == 'l':
-			final_prob *= get_prob(LUCK, i, num_previous_luck_falls)
+		prob = get_prob(LUCK, i, num_previous_luck_falls)
+		if prob > 1.0:
+			return 0.0
+		elif element == 'l':
+			final_prob *= prob
 			num_previous_luck_falls += 1
 		elif element == 'u':
-			final_prob *= (1 - get_prob(LUCK, i, num_previous_luck_falls))
+			final_prob *= (1.0 - prob)
 		else:
 			print "Something is not right"
 	# print sequence
+	# print final_prob
+	if final_prob == 1.0:
+		print "Control didn't enter the for loop in get_prob_sequence"
 	# print final_prob
 	return final_prob
 
@@ -54,9 +64,15 @@ def get_prob_luck_falls_x_times(x):
 	for i in range(x, TOTAL_TURNS):
 		sequence_falls.append('u')
 	for permuted_sequence in perm_unique(sequence_falls):
-		total_prob += get_prob_sequence(permuted_sequence)
+		# print permuted_sequence
+		total_prob += get_prob_sequence(permuted_sequence, x)
 	return total_prob
 
+# sum = 0.0
+# for i in range(0, TOTAL_TURNS + 1):
+# 	sum = sum + get_prob_luck_falls_x_times(i)
+# 	print i, " = ", get_prob_luck_falls_x_times(i)
+# print "sum = ", sum
 lucky_times = 0.0
 for i in range(1, TOTAL_TURNS + 1):
 	lucky_times += i * get_prob_luck_falls_x_times(i)
